76b2f55712055948e79efade2982a863d83cd697
mofortin <fortin.mo@gmail.com>
2016-09-15 18:39:54 -0400

    DEV 1.0.0

---
+++ src/com/codingforcookies/armorequip/Main.java
@@ -0,0 +1,13 @@
+package com.codingforcookies.armorequip;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main
+        extends JavaPlugin
+{
+    public void onEnable()
+    {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new ArmorListener(getConfig().getStringList("blocked")), this);
+    }
+}
